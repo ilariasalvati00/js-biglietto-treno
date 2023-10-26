08b1d8d66f34b4b10520c2d6284e764a5d4c1c5a
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-10-26 22:14:49 +0200

    inizio esercizio

---
+++ main.js
@@ -0,0 +1,9 @@
+let km = prompt("dimmi il numero dei chilometri da percorrere", "0")
+let eta = prompt("dimmi l'età del passeggero" , "21")
+
+km = parseInt(km)
+eta = parseInt(età)
+
+let prezzo = 0.21*km
+
+console.log(km)
