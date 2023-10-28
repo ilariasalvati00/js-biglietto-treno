283460b7320115c31ad8be204cc5f09cd031eb59
Ilaria Salvati <ilariasalvati00@gmail.com>
2023-10-28 23:19:49 +0200

    Esercizio completo

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-let km = prompt("dimmi il numero dei chilometri da percorrere", "0")
-let eta = prompt("dimmi l'età del passeggero" , "21")
+const km = Number(prompt("Quanti chilometri?"));
+if (! isNaN(km)){
+    let prezzo_biglietto = 0.21*km;
 
-km = parseInt(km)
-eta = parseInt(età)
+    const eta = Number(prompt("Quanti anni hai?"));
+    if (! isNaN(eta)){
 
-let prezzo = 0.21*km
-
-console.log(km)
+        let messaggio_sconto = "";
+        if (eta<18) {
+            prezzo_biglietto = prezzo_biglietto*0.8;
+            messaggio_sconto = "Sconto minorenni."
+        } else if (eta>65) {
+            prezzo_biglietto = prezzo_biglietto*0.6;
+            messaggio_sconto = "Sconto anziani."
+        }
+        
+        prezzo_biglietto = prezzo_biglietto.toFixed(2);
+        
+        document.getElementById("prezzo").innerHTML= messaggio_sconto + "Il prezzo del biglietto è €" + prezzo_biglietto;
+    } else {
+        console.log("Non hai inserito un'età corretta");
+    }
+    
+} else {
+    console.log("Non hai inserito un numero di chilometri valido");
+}
